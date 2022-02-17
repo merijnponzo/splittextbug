57c3d423a4e91789dac8846644f04ba4653bff53
merren <merijn@ponzo.nl>
2022-02-17 20:43:18 +0100

    first commit
    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <h1>Splittext bug</h1>
+    <div ref="typewriter">Splittext bug typewriter</div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  created() {
+    const line = new this.$SplitText(this.$refs.typewriter, {
+      type: "words",
+      position: "absolute",
+    });
+    /*
+    const line = new this.$SplitText(this.$refs.typewriter, {
+      type: "words",
+      position: "absolute",
+    });
+    */
+  },
+};
+</script>
